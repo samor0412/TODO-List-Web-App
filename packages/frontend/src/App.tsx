4e8e8ace3d6e5f8ef67b281ee16b2d4bc5fa8d63
samor0412 <samor412@gmail.com>
2024-07-04 22:10:41 +0800

    feat(frontend): init

---
+++ packages/frontend/src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="relative overflow-hidden bg-white">placeholder</div>
+  )
+}
+
+export default App
---
+++ packages/frontend/vite.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config
+export default defineConfig({
+  server: {
+    port: parseInt(process.env.PORT ?? '5173', 10),
+  },
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: '.vitest/setup',
+    include: ['**/test.{ts,tsx}']
+  }
+})
