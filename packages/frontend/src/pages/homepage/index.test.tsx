f45d13adeba94b8c39f05adc8cc1258dcd448e19
samor0412 <samor412@gmail.com>
2024-07-04 22:56:19 +0800

    feat(frontend): homepage display

--- packages/frontend/src/pages/homepage/index.test.tsx
+++ packages/frontend/src/pages/homepage/index.test.tsx
@@ -1,7 +1,7 @@
 import {render, screen} from '@testing-library/react'
 import HomePage from '.'
 
-describe.skip('HomePage', () => {
+describe('HomePage', () => {
     it('should render', () => {
         render(<HomePage/>)
 
--- packages/frontend/src/pages/homepage/index.tsx
+++ packages/frontend/src/pages/homepage/index.tsx
@@ -1,6 +1,9 @@
 const HomePage: React.FC = () => {
     return (
-        <div></div>
+        <div className="flex flex-col items-center h-screen w-screen pt-10">
+            <h2 className="text-4xl font-bold my-24">Todo Webapp</h2>
+            <button className="btn btn-primary">Create New Todo Page</button>
+        </div>
     )
 }
 
